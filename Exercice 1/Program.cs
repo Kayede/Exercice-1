d11b701b48ba796a376ede47be200e7e00617ce7
antne <antne@DESKTOP-KGFTPNS>
2021-10-14 13:44:13 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prenom, nom;
+            int age;
+
+            // Saisie des trois infos
+            Console.Write("Entrez votre prénom : ");
+            prenom = Console.ReadLine();
+            Console.Write("Entrez votre nom : ");
+            nom = Console.ReadLine();
+            Console.Write("Quel est votre âge ? ");
+            age = int.Parse(Console.ReadLine());
+
+            // Affichage
+            Console.WriteLine("Bonjour " + prenom + " " + nom + ", vous avez " + age + " ans.");
+            Console.ReadLine();
+        
+        }
+    }
+}
